8a4264edf86ba40cb398cf0a21c02b1e493638df
junasliang <jun942670@gmail.com>
2025-08-02 14:55:13 +0800

    fix: true random

--- goCards/deck.go
+++ goCards/deck.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -32,8 +33,12 @@ func deal(d deck, handSize int) (deck, deck) {
 
 // shuffle a deck of cards
 func (d deck) shuffle() {
+	// true random, see rand doc
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1) //psuedo random
+		newPosition := r.Intn(len(d) - 1)
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
