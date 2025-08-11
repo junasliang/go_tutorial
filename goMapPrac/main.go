7e5d172f4acae9390f019c8267017798583d900b
junasliang <jun942670@gmail.com>
2025-08-11 09:11:11 +0800

    feat: map pratice

--- goMapPrac/main.go
+++ goMapPrac/main.go
@@ -3,13 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#00ff00",
-	// 	"blue":  "#0000ff",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+	}
 
-	colors := make(map[string]string)
+	// colors := make(map[string]string)
 	colors["white"] = "#ffffff"
-	fmt.Println(colors)
+	colors["black"] = "#000000"
+
+	// delete(colors, "black")
+	// fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex for ", color, " is ", hex)
+	}
 }
